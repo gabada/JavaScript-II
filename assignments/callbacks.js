2db438711081efbda1c95af048b226f15569e644
Gill Abada <gill@callifi.com>
2019-01-22 16:30:47 -0500

    clean up problem 5 on callbacks.js.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -60,7 +60,8 @@ multiplyNums(3, 10, function(result){
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  cb(list.includes(item))};
+  return cb(list.includes(item));
+}
 
 contains("Gum", items, function(doesIt){
   console.log(doesIt);
