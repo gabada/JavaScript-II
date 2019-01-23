c8c7c67e55a3c6c5af2eb1f9791f8248fc83aaf9
Gill Abada <gill@callifi.com>
2019-01-22 19:36:23 -0500

    add stretch for closure.js.

--- assignments/closure.js
+++ assignments/closure.js
@@ -31,7 +31,21 @@ console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let privateCounter = 0;
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  return {
+    increment() {
+      return ++privateCounter;
+    },
+    decrement(){
+      return  --privateCounter;
+    }
+  }
 };
+
+let counterObject = counterFactory();
+console.log(counterObject.increment());
+console.log(counterObject.decrement());
+console.log(counterObject.decrement());
